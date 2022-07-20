f65466b19c4664418e6113ca829290466acef82d
iamhge <fine632@gmail.com>
2022-07-20 09:39:37 -0400

    [ADD] (#30) - add spring data jpa method in SensorRepository

--- server/src/main/java/com/iieee/server/domain/sensor/SensorRepository.java
+++ server/src/main/java/com/iieee/server/domain/sensor/SensorRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
     List<Sensor> findByDateTimeBetweenAndStation(LocalDateTime start, LocalDateTime end, Station station);
+    Sensor findTopByStation(Station station);
 }
