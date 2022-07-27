bcbd35b4b8001d179a50a9dcd7cf50fb6c8eb6b5
iamhge <fine632@gmail.com>
2022-07-27 15:07:04 -0400

    [ADD] (#42) - add interface to find station by eui in repository

--- server/src/main/java/com/iieee/server/domain/station/StationRepository.java
+++ server/src/main/java/com/iieee/server/domain/station/StationRepository.java
@@ -3,6 +3,9 @@ package com.iieee.server.domain.station;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
+    Optional<Station> findByEui(String eui);
 }
